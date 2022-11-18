772dca743404f6fb1073a5cdfe3f3fb15bbce24e
absolutty <ada.hajro@gmail.com>
2022-11-18 11:42:40 +0100

    Initial commit.

---
+++ src/test/java/sk/uniza/fri/customerjpa/CustomerJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.uniza.fri.customerjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
